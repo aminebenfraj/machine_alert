f792aa7f3ea3c7f0812321e4d93a2906c11d56a8
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-04-24 09:18:59 +0200

    Update API URLs to use production endpoint

--- frontend/src/apis/logistic/callApi.js
+++ frontend/src/apis/logistic/callApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/call"
+const API_URL = "https://machine-alert.onrender.com"
 
 // Get all calls with optional filtering
 export const getCalls = async (filters = {}) => {
