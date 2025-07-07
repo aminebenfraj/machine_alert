e3896ddfb80c8ecea425725ccbf4311d779c4450
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-07-07 11:36:50 +0200

    fix: update axios base URL to production environment

--- frontend/src/apis/api.jsx
+++ frontend/src/apis/api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000/`, // Update with your backend URL
+  baseURL: `https://machine-alert.onrender.com/`,
   timeout: 5000,
 });
 
